50c796fe9f68118e43b56bef6ead6fae23df5093
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2017-10-26 16:09:10 +0700

    mengekases data dari backend java

--- blogs-api/src/main/java/com/dimas/maryanto/blogs/BlogApiApplication.java
+++ blogs-api/src/main/java/com/dimas/maryanto/blogs/BlogApiApplication.java
@@ -2,11 +2,25 @@ package com.dimas.maryanto.blogs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class BlogApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApiApplication.class, args);
+    }
+
+    @Configuration
+    @EnableWebMvc
+    public class WebConfig extends WebMvcConfigurerAdapter{
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/api/**").allowedOrigins("http://localhost:4200");
+        }
     }
 }
--- blogs-client/src/app/app.component.ts
+++ blogs-client/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Http} from '@angular/http';
+import {Subscription} from 'rxjs/Subscription';
+import {Tag} from './tag.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'app';
+  onSubscribe: Subscription;
+  tagContent: {
+    content: Tag[],
+    last: boolean,
+    totalPages: number,
+    totalElements: number,
+    numberOfElements: number,
+    first: boolean,
+    size: number,
+    number: number
+  };
+
+  ngOnInit(): void {
+    this.onSubscribe = this.http.get('http://localhost:8080/api/tags/list').subscribe(
+      data => this.tagContent = data.json(),
+      error => console.error(error)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.onSubscribe.unsubscribe();
+  }
+
+  constructor(private http: Http) {
+
+  }
 }
