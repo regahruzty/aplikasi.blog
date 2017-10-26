6ff9b58afcad0e60a5cb00c56fddc1da61b90358
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2017-10-26 11:16:59 +0700

    membuat page

---
+++ blogs-api/src/main/java/com/dimas/maryanto/blogs/model/Page.java
@@ -0,0 +1,43 @@
+package com.dimas.maryanto.blogs.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "pages")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "listTags")
+public class Page {
+
+    @Id
+    @GenericGenerator(name = "gen_page", strategy = "uuid2")
+    @GeneratedValue(generator = "gen_page")
+    private String id;
+
+    @Column(length = 50, nullable = false)
+    private String judul;
+
+    @Column(length = 150)
+    private String message;
+
+    @Lob
+    private String description;
+
+    @Column(nullable = false, name = "img_url")
+    private String logoUrl;
+
+    @OneToMany
+    @JoinTable(name = "tag_pages",
+            joinColumns = @JoinColumn(name = "page_id", nullable = false),
+            inverseJoinColumns = @JoinColumn(name = "tag_id", nullable = false))
+    private List<Tag> listTags = new ArrayList<>();
+}
---
+++ blogs-api/src/main/java/com/dimas/maryanto/blogs/model/Tag.java
@@ -0,0 +1,24 @@
+package com.dimas.maryanto.blogs.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tags")
+public class Tag {
+
+    @Id
+    @GenericGenerator(name = "gen_tag", strategy = "uuid2")
+    @GeneratedValue(generator = "gen_tag")
+    private String id;
+
+    @Column(length = 20, nullable = false, unique = true)
+    private String nama;
+}
